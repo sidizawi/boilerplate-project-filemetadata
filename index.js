967f9ba8330dd4d3bef60af78462b8e270aad3db
sidi <zawisidim@gmail.com>
2023-07-30 15:56:07 +0200

    finish

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +12,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: process.cwd() + "/uploads" })
 
+app.post('/api/fileanalyse', upload.single("upfile"),  function (req, res) {
+
+  console.log(req.file, req.body)
+
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
